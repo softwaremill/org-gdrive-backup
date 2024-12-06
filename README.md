998270b744eb154cfd44a0e5bbef2364478ab413
Jakub Papuga <mrpsycho@mrpsycho.pl>
2024-12-06 16:51:28 +0100

    Add compression

--- README.md
+++ README.md
@@ -36,17 +36,19 @@ This project is a simple Python script that backs up all Google Drives in a Goog
 
 | Name                     | Required | Purpose                                                                    | Type   | Default                  |
 |--------------------------|----------|----------------------------------------------------------------------------|--------|--------------------------|
-| `DELEGATED_ADMIN_EMAIL`    | Yes      | E-mail address of the superadmin account                                   | string |                          |
-| `WORKSPACE_CUSTOMER_ID`    | Yes      | Customer ID from Google Admin Console                                      | string |                          |
-| `SERVICE_ACCOUNT_FILE`     | Yes      | Path to service account .json key                                          | string | service-account-key.json |
-| `MAX_QUERY_THREADS`        | No       | Number of threads used to list files (always one thread per Google Drive)  | int    | 5                        |
-| `MAX_DOWNLOAD_PROCESSES`   | No       | Number of max processes used to download files from Google Drive           | int    | cpu_count()              |
-| `FILES_PER_DOWNLOAD_BATCH` | No       | How many files should a process download                                   | int    | 1                        |
+| `DELEGATED_ADMIN_EMAIL`    | Yes      | E-mail address of the superadmin account                                   | string |                            |
+| `WORKSPACE_CUSTOMER_ID`    | Yes      | Customer ID from Google Admin Console                                      | string |                            |
+| `SERVICE_ACCOUNT_FILE`     | Yes      | Path to service account .json key                                          | string | `service-account-key.json` |
+| `MAX_QUERY_THREADS`        | No       | Number of threads used to list files (always one thread per Google Drive)  | int    | `5`                        |
+| `MAX_DOWNLOAD_PROCESSES`   | No       | Number of max processes used to download files from Google Drive           | int    | `cpu_count()`              |
+| `FILES_PER_DOWNLOAD_BATCH` | No       | How many files should a process download                                   | int    | `1`                        |
+| `COMPRESS_DRIVES`          | No       | Compress the exported drives to a .zip file                                 | bool   | `false`                   |
 
 
 # Roadmap
 
-- [ ] (Configurable) compression
+- [x] File compression
+- [ ] Configurable algorithm for file compression
 - [ ] Configurable export formats
 - [ ] Configurable metadata fields
 - [ ] Configurable links behaviour
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 import time
 from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
+import pgzip
+import shutil
 
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
@@ -15,6 +17,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 MAX_QUERY_THREADS = int(os.getenv("MAX_QUERY_THREADS", 5))
 MAX_DOWNLOAD_PROCESSES = int(os.getenv("MAX_DOWNLOAD_PROCESSES", cpu_count()))
 FILES_PER_DOWNLOAD_BATCH = int(os.getenv("FILES_PER_DOWNLOAD_BATCH", 1))
+COMPRESSION_ALGORITHM = os.getenv("COMPRESSION_ALGORITHM", "pigz")
+PIGZ_COMPRESSION_PROCESSES = int(os.getenv("COMPRESSION_PROCESSES", cpu_count()))
 
 SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE", "service-account-key.json")
 SCOPES = ["https://www.googleapis.com/auth/admin.directory.user.readonly", 
@@ -22,8 +26,9 @@ SCOPES = ["https://www.googleapis.com/auth/admin.directory.user.readonly",
           "https://www.googleapis.com/auth/drive.readonly"]
 DELEGATED_ADMIN_EMAIL = os.getenv("DELEGATED_ADMIN_EMAIL", "xxx")
 WORKSPACE_CUSTOMER_ID = os.getenv("WORKSPACE_CUSTOMER_ID", "xxx")
-random.seed(time.time())
+COMPRESS_DRIVES = os.getenv("COMPRESS_DRIVES", "false").lower() == "true"
 
+random.seed(time.time())
 
 
 def get_credentials(subject):
@@ -77,13 +82,11 @@ def get_files_shared(drive_service, drive_id):
 
 def get_file_path(drive_service, file_id, supportsAllDrives=False):
     file_path = []
-    print(f"Processing file {file_id}")
 
     file = drive_service.files().get(fileId=file_id, fields="name, parents", supportsAllDrives=supportsAllDrives).execute()
     file_path.append(file['name'])
 
     parents = file.get('parents', [])
-    print(f"Parents: {parents}")
     while parents:
         parent_id = parents[0]
         parent = drive_service.files().get(fileId=parent_id, fields="name, parents", supportsAllDrives=supportsAllDrives).execute()
@@ -224,7 +227,7 @@ def chunkify(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-def download():
+def download(compress=False):
     drives = os.listdir("metadata")
     random.shuffle(drives) # In case of failure, we should have at least some data
 
@@ -232,23 +235,39 @@ def download():
         with open(f"metadata/{drive}", "r") as f:
             files = json.load(f)
 
-        if drive.startswith("u_"):
-            user_name = drive[2:-5]
-            credentials = get_credentials(user_name)
-            with Pool(processes=MAX_DOWNLOAD_PROCESSES) as pool:
-                args = [(batch, user_name, credentials) for batch in chunkify(files, FILES_PER_DOWNLOAD_BATCH)]
-                with tqdm(total=len(files), desc=f"Downloading files for {user_name}") as pbar:
-                    for _ in pool.imap_unordered(process_file, args):
-                        pbar.update(FILES_PER_DOWNLOAD_BATCH)
+        drive_id = drive[2:-5]
+        credentials = get_credentials(DELEGATED_ADMIN_EMAIL)
+        with Pool(processes=MAX_DOWNLOAD_PROCESSES) as pool:
+            args = [(batch, drive_id, credentials) for batch in chunkify(files, FILES_PER_DOWNLOAD_BATCH)]
+            with tqdm(total=len(files), desc=f"Downloading files for {drive_id}") as pbar:
+                for _ in pool.imap_unordered(process_file, args):
+                    pbar.update(FILES_PER_DOWNLOAD_BATCH)
+        
+        if compress and len(files) > 0:
+            compress_drive(drive_id, COMPRESSION_ALGORITHM)
+            # compress_folder(f"metadata/{drive}", "lz4")
 
-        elif drive.startswith("s_"):
-            drive_id = drive[2:-5]
-            credentials = get_credentials(DELEGATED_ADMIN_EMAIL)
-            with Pool(processes=MAX_DOWNLOAD_PROCESSES) as pool:
-                args = [(batch, drive_id, credentials) for batch in chunkify(files, FILES_PER_DOWNLOAD_BATCH)]
-                with tqdm(total=len(files), desc=f"Downloading files for {drive_id}") as pbar:
-                    for _ in pool.imap_unordered(process_file, args):
-                        pbar.update(FILES_PER_DOWNLOAD_BATCH)
+def compress_drive(drive_id, algorithm="lz4"):
+    print(f"Compressing {drive_id} with {algorithm}")
+    start_time = time.time()
+    original_size = os.path.getsize(f"files/{drive_id}")
+
+    match algorithm:
+        case "lz4":
+            tar_path = f"files/{drive_id}.tar.lz4"
+            success = os.system(f"tar c - -C files {drive_id} | lz4 - {tar_path}")
+        case "pigz":
+            tar_path = f"files/{drive_id}.tar.gz"
+            success = os.system(f"tar cf - -C files {drive_id} | pigz -p {PIGZ_COMPRESSION_PROCESSES} > {tar_path}")
+        case _:
+            raise NotImplementedError(f"Compression algorithm {algorithm} not implemented")
+        
+    shutil.rmtree(f"files/{drive_id}")
+        
+    print(f"Compression finished in {time.time() - start_time:.2f}s")
+    compressed_size = os.path.getsize(tar_path)
+    print(f"Compression ratio: {original_size / compressed_size:.2f}")
+
 
 
 def main():
@@ -263,7 +282,7 @@ def main():
     with ThreadPoolExecutor(max_workers=MAX_QUERY_THREADS) as executor:
         futures = [executor.submit(save_shared_file_list, drive) for drive in shared_drives]
 
-    download()
+    download(compress=COMPRESS_DRIVES)
 
 
 if __name__ == "__main__":
