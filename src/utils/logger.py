d93a48ebb5b79a5609255e37eee63b3588d5c64a
Jakub Papuga <mrpsycho@mrpsycho.pl>
2025-01-13 12:01:49 +0100

    Handle lowercase log_level

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,7 +5,7 @@ import os
 logger.remove(0)
 logger.add(
     sink=sys.stdout,
-    level=os.getenv("LOG_LEVEL", "INFO"),
+    level=os.getenv("LOG_LEVEL", "INFO").upper(),
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <level>{message}</level>",
     colorize=True,
 )
